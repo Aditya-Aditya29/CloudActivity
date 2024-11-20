f730fe1d2b147af92f592748d486b4c2be767cb8
Aditya Aditya <adicanada.1088@gmail.com>
2024-11-20 18:36:05 -0500

    done2

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <h2>Group 7 activity</h2>
         <h3>Group Members</h3>
         <p>Aditya - c0912729</p>
+        <p>Dissanayaka Mudiyanselage - C0914949</p>
+        <p>Sandeep Kaur - C0904166</p>
+        <p>Damanpreet Kaur - C0903021</p>
+        <p>Roopdeep Kaur - C0912740</p>
       </div>
     </div>
   );
