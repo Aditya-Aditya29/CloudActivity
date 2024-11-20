e1cb3e8dc881261d6baeaa41a0df4c06a71c5644
Aditya Aditya <adicanada.1088@gmail.com>
2024-11-20 18:24:50 -0500

    done

---
+++ src/App copy.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>Welcome</h1>
+        <h2>Group 7 activity</h2>
+        <h3>Group Members</h3>
+        <p>Aditya - c0912729</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
